db73025242e78a29adce485de343c97eb1cf31f1
mohanadabulatifa <mohanad.abulatifa@gmail.com>
2025-09-17 13:36:08 +0200

    arreglar que choquen el puerto de los tests y jenkins

--- src/test/java/org/example/steps/CucumberSpringConfig/CucumberSpringConfiguration.java
+++ src/test/java/org/example/steps/CucumberSpringConfig/CucumberSpringConfiguration.java
@@ -3,11 +3,13 @@ package org.example.steps.CucumberSpringConfig;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.example.api.Application;
+import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = Application.class
 )
+@TestPropertySource("classpath:application-test.properties")
 public class CucumberSpringConfiguration {
 }
