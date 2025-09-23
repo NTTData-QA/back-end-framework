0e8a309e436c5cd903feac0d0653b08d49f12e37
Julián Manzano <jmanzanm@emeal.nttdata.com>
2025-09-23 12:16:05 +0200

    fix: account delete methods

--- src/test/java/org/example/steps/specific/AccountsSteps.java
+++ src/test/java/org/example/steps/specific/AccountsSteps.java
@@ -15,99 +15,94 @@ import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AccountsSteps extends AbstractSteps {
-  private Response response;
-  private static String jwt;
-  private BankService bankService = testContext().getBankService();
-  private BankAPI proxy = bankService.proxy;
+    private Response response;
+    private static String jwt;
+    private BankService bankService = testContext().getBankService();
+    private BankAPI proxy = bankService.proxy;
 
 
-  @When("i request this users account information")
-  public void iRequestThisUsersAccountInformation() {
-    response = proxy.getUserAccounts(null);
-    testContext().setResponse(response);
-  }
-
-  @When("i request this users account amount")
-  public void iRequestThisUsersAccountAmount() {
-    response =proxy.getUserAmount(null);
-    Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
-  }
-
-  @Then("i should receive the amount")
-  public void iShouldReceiveTheAmount() {
-    String amount = response.readEntity(String.class);
-    System.out.println("The amount of the logged user is ".concat(amount).concat(" euros"));
-  }
-
-  @And("The customer creates {int} account with {double} euros each")
-  public void theCustomerCreatesAccountWithEurosEach(int numberOfAccount, double euros) {
-
-    while(numberOfAccount>0) {
-      Account account = new Account();
-      account.setAmount(euros);
-      account.setAccountType(Account.AccountType.BUSINESS_ACCOUNT);
-      Response accountResponse = bankService.doNewAccount(account,null);
-
-      Assert.assertEquals(201,accountResponse.getStatus());
-      numberOfAccount--;
-      String accountOrigin = accountResponse.readEntity(String.class);
-      String[] parts = accountOrigin.split(": ");
-
-      // Extraer el número como String y luego convertirlo a un número entero
-      String accountIdString = parts[1];
-      int accountId = Integer.parseInt(accountIdString);
-      testContext().setOriginID(accountId);
+    @When("i request this users account information")
+    public void iRequestThisUsersAccountInformation() {
+        response = proxy.getUserAccounts(null);
+        testContext().setResponse(response);
     }
 
-  }
+    @When("i request this users account amount")
+    public void iRequestThisUsersAccountAmount() {
+        response = proxy.getUserAmount(null);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
 
-  @And("The receiving customer has an account with id {int}")
-  public void theReceivingCustomerHasAnAccountWithId(int receiverAccountId) {
-    proxy = bankService.proxy;
-    Response receiverAccountresponse = proxy.accountById(receiverAccountId);
-    Assert.assertEquals(200,receiverAccountresponse.getStatus());
-  }
+    @Then("i should receive the amount")
+    public void iShouldReceiveTheAmount() {
+        String amount = response.readEntity(String.class);
+        System.out.println("The amount of the logged user is ".concat(amount).concat(" euros"));
+    }
 
-  @Given("the customer creates an account with {double} euros with status blocked {string}")
-  public void theCustomerCreatesAnAccountWithEurosWithStatusBlocked(double euros, String stBlocked) {
-    // TODO divide create account function and block account function when available
-    // ==> Given the customer creates an account with 200 euros
-    // When the customer blocks the account
-    // And the customer tries to delete the account
-    Boolean blocked = Boolean.parseBoolean(stBlocked);
-    System.out.println(blocked);
+    @And("The customer creates {int} account with {double} euros each")
+    public void theCustomerCreatesAccountWithEurosEach(int numberOfAccount, double euros) {
 
-    Account account = new Account();
-    account.setAmount(euros);
-    account.setAccountType(Account.AccountType.BUSINESS_ACCOUNT);
-    account.setIsBlocked(blocked);
+        while (numberOfAccount > 0) {
+            Account account = new Account();
+            account.setAmount(euros);
+            account.setAccountType(Account.AccountType.BUSINESS_ACCOUNT);
+            Response accountResponse = bankService.doNewAccount(account, null);
 
-    System.out.println(account.toString());
-    Response accountResponse = bankService.doNewAccount(account,null);
-    Assert.assertEquals(201,accountResponse.getStatus());
+            Assert.assertEquals(201, accountResponse.getStatus());
+            numberOfAccount--;
+            String accountOrigin = accountResponse.readEntity(String.class);
+            String[] parts = accountOrigin.split(": ");
 
-    String accountOrigin = accountResponse.readEntity(String.class);
-    String[] parts = accountOrigin.split(": ");
-    String accountIdString = parts[1];
-    int accountId = Integer.parseInt(accountIdString);
-    testContext().setOriginID(accountId);
+            // Extraer el número como String y luego convertirlo a un número entero
+            String accountIdString = parts[1];
+            int accountId = Integer.parseInt(accountIdString);
+            testContext().setOriginID(accountId);
+        }
 
+    }
 
-  }
+    @And("The receiving customer has an account with id {int}")
+    public void theReceivingCustomerHasAnAccountWithId(int receiverAccountId) {
+        proxy = bankService.proxy;
+        Response receiverAccountresponse = proxy.accountById(receiverAccountId);
+        Assert.assertEquals(200, receiverAccountresponse.getStatus());
+    }
 
-  @When("the customer tries to delete the account")
-  public void theCustomerTriesToDeleteTheAccount() {
-    Integer accountId = testContext().getOriginID();
-    Response accountResponse = proxy.accountById(accountId);
-    System.out.println(accountResponse.readEntity(String.class));
-    Response deleteResponse = bankService.doDeleteAccountById(accountId);
-    testContext().setResponse(deleteResponse);
-  }
+    @Given("the customer creates an account with {double} euros")
+    public void theCustomerCreatesAnAccountWithEuros(double euros) {
 
-  @Then("i should receive the code {int} and a message")
-  public void iShouldReceiveTheCodeAndAMessage(int codigo) {
-    response = testContext().getResponse();
-    System.out.println(response.readEntity(String.class));
-    Assert.assertEquals(codigo, response.getStatus());
-  }
+        Account account = new Account();
+        account.setAmount(euros);
+        account.setAccountType(Account.AccountType.BUSINESS_ACCOUNT);
+
+        Response accountResponse = bankService.doNewAccount(account, null);
+        Assert.assertEquals(201, accountResponse.getStatus());
+
+        String accountOrigin = accountResponse.readEntity(String.class);
+        String[] parts = accountOrigin.split(": ");
+        String accountIdString = parts[1];
+        int accountId = Integer.parseInt(accountIdString);
+        testContext().setOriginID(accountId);
+    }
+
+    @And("the customer blocks the account")
+    public void theCustomerBlocksTheAccount() {
+        Integer accountId = testContext().getOriginID();
+        Response blockedResponse = bankService.doUpdateBlockStatus(accountId, true);
+        testContext().setResponse(blockedResponse);
+    }
+
+    @When("the customer tries to delete the account")
+    public void theCustomerTriesToDeleteTheAccount() {
+        Integer accountId = testContext().getOriginID();
+        Response deleteResponse = bankService.doDeleteAccountById(accountId);
+        testContext().setResponse(deleteResponse);
+    }
+
+    @Then("the customer should receive the code {int} and a message")
+    public void theCusromerShouldReceiveTheCodeAndAMessage(int codigo) {
+        response = testContext().getResponse();
+        Assert.assertEquals(codigo, response.getStatus());
+    }
+
 }
--- src/test/resources/features/accountsDelete.feature
+++ src/test/resources/features/accountsDelete.feature
@@ -4,21 +4,23 @@ Feature: Check BOOLEANS before deleting Accounts by accountId
     Given the system is ready and i log with email "john.doe@example.com" and password "password123"
 
   Scenario: Delete recently created account
-    Given the customer creates an account with 200 euros with status blocked "false"
+    Given the customer creates an account with 200 euros
     When the customer tries to delete the account
-    Then i should receive the code 200 and a message
+    Then the customer should receive the code 200 and a message
 
   Scenario: Delete blocked account
-    Given the customer creates an account with 200 euros with status blocked "true"
-    And the customer tries to delete the account
-    Then i should receive the code 400 and a message
+    Given the customer creates an account with 200 euros
+    And the customer blocks the account
+    When the customer tries to delete the account
+    Then the customer should receive the code 400 and a message
 
   Scenario: Delete account in debt
-    Given the customer creates an account with -200 euros with status blocked "false"
+    Given the customer creates an account with -200 euros
     When the customer tries to delete the account
-    Then i should receive the code 400 and a message
+    Then the customer should receive the code 400 and a message
 
   Scenario: Delete account in debt and blocked
-    Given the customer creates an account with -200 euros with status blocked "true"
-    And the customer tries to delete the account
-    Then i should receive the code 400 and a message
+    Given the customer creates an account with -200 euros
+    And the customer blocks the account
+    When the customer tries to delete the account
+    Then the customer should receive the code 400 and a message
